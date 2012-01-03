5d64b1ab53fa225eeeb7b6a343280662892a2f3b
Chris Beams <cbeams@vmware.com>
2012-01-03 14:32:54 +0100

    Fix deprecated access to classesDir

--- bundlor-plugin/src/main/groovy/org/springframework/build/bundlor/BundlorPlugin.groovy
+++ bundlor-plugin/src/main/groovy/org/springframework/build/bundlor/BundlorPlugin.groovy
@@ -94,7 +94,7 @@ public class BundlorPlugin implements Plugin<Project> {
                 // execute the ant task, and write out the manifest file
                 project.ant.bundlor(
                         enabled: enabled,
-                        inputPath: project.sourceSets.main.classesDir,
+                        inputPath: project.output.classesDir,
                         outputPath: outputDir,
                         bundleVersion: bundleVersion,
                         failOnWarnings: failOnWarnings) {
